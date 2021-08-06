2a9a60c62d14976bb3b6dabb54c52b473f3b5a52
SSGSSACE <quangbayhai@gmail.com>
2021-08-06 19:26:46 +0700

    August 6, 2021 7:26 PM

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace GopFile
             string[] files = Directory.GetFiles(path, "*.csv", SearchOption.TopDirectoryOnly);
             // using (var output = File.Create(path + "output.csv"))
             // {
-                var data = File.ReadAllLines("../FILE2/20210719log - Copy1");
+                var data = File.ReadAllLines("D:/BT C#/GopFile/FILE2/20210719log - Copy1");
                 foreach (var file in data)
                 {
                     // using (var data=File.ReadAllLines(file))
