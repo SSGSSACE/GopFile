75921d756b9a69f136d68426c2cb323bc8239841
SSGSSACE <quangbayhai@gmail.com>
2021-08-06 19:09:30 +0700

    August 6, 2021 7:09 PM

--- Program.cs
+++ Program.cs
@@ -9,6 +9,16 @@ namespace GopFile
             Console.WriteLine("Hello World!");
             string path = @"D:/BT C#/GopFile/FILE2/";
             string[] files = Directory.GetFiles(path, "*.csv", SearchOption.TopDirectoryOnly);
+            using (var output = File.Create(path + "output.csv"))
+            {
+                foreach (var file in files)
+                {
+                    using (var data=File.OpenRead(file))
+                    {
+                        data.CopyTo(output);
+                    }
+                }
+            }
         }
     }
 }
