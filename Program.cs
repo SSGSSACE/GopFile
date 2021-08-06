7d346a04e00643654ec79d4c9ed54e26391c35fd
SSGSSACE <quangbayhai@gmail.com>
2021-08-06 23:26:46 +0700

    August 6, 2021 11:26 PM

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace GopFile
                 // using (var data=File.ReadAllLines(file))
                 // {
                 // data.CopyTo(output);
-                Console.WriteLine(file.GetType());
+                Console.WriteLine(file);
                     // }
                 }
             // }
