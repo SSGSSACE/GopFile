fb7b5e1f0d118542a64a47ee8cbc8d0607393a9a
SSGSSACE <quangbayhai@gmail.com>
2021-08-06 18:57:45 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+namespace GopFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string path = @"D:/BT C#/GopFile/FILE2/";
+            string[] files = Directory.GetFiles(path, "*.csv", SearchOption.TopDirectoryOnly);
+        }
+    }
+}
